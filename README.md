574ff460d733e50b20ab2376716f7364b2a2eee9
Annika <annika0uwu@gmail.com>
2021-09-30 23:19:16 -0700

    Write code and documentation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Rust JSON parsing benchmarks
-This project aims to provide benchmarks to show how various JSON-parsing libraries in the Rust programming language perform at various JSON-parsing tasks.
+This project aims to provide benchmarks to show how various JSON-parsing libraries in the Rust programming language perform at various JSON-parsing tasks. It is only concerned with performance, and does not take into account other factors that you should consider when choosing a JSON parser (such as validation, RAM usage, code readability, and active maintainace).
 ## Benchmark methodology
 The following Rust libraries for JSON parsing were benchmarked:
 - [Serde JSON](https://crates.io/crates/serde_json)
@@ -7,16 +7,47 @@ The following Rust libraries for JSON parsing were benchmarked:
 - [A-JSON](https://crates.io/crates/ajson)
 - [json-rust](https://crates.io/crates/json)
 - [Pikkr](https://crates.io/crates/pikkr_annika)
+- [simd-json](https://crates.io/crates/simd-json)
+- [tinyjson](https://crates.io/crates/tinyjson)
 
 For each library, each of the following tasks was benchmarked both on a large and a small JSON object (which can be found in the `json/` folder):
 - Retrieving the value of a single top-level property
 - Retrieving the value of a single fourth-level property
 - Parsing the entire JSON object
 ## Benchmarking process
-The benchmarks were taken under macOS 11.6 and rustc 1.57.0-nightly (8f8092cc3 2021-09-28) on a system with 16 GB of 2133 MHz RAM and a 2.4 GHz Intel i5-8279U CPU (with 4 physical and 8 logical cores). The benchmarks were all run at the same time, with minimal background processes.
+The benchmarks were taken under macOS 11.6 and rustc 1.57.0-nightly (8f8092cc3 2021-09-28) on a system with 16 GB of 2133 MHz RAM and a 2.4 GHz Intel i5-8279U CPU (with 4 physical and 8 logical cores).
+The benchmarks were run sequentially with output redirected to a text file (`cargo bench > bench.txt`), with minimal background processes.
+## Results
+There are two main types of JSON parsing library:
+- _property-parsing libraries_ (Serde JSON, json-rust, simd-json, and tinyjson), which focus on retrieving the value of a particular JSON property, and have little to no ability to parse an entire JSON file at once
+- _object-parsing libraries_ (GJSON, A-JSON, and Pikkr), which parse an entire JSON object/file at once, and subsequently allow access to any value within the JSON file with minimal overhead
+Each of these library types has different applications; you need to figure out which one is best for your use case.
+
+Although I did my best to implement each benchmark for every library, the most relevant benchmarks for the property-parsing libraries are the `top_level` and `fourth_level` benchmarks (which retrieve a single value).
+Conversely, the most relevant benchmark for the object-parsing libraries is the `parse_all` benchmark; the other benchmarks will take about the same time, since these libraries must parse the entire object before retrieving any values.
+
+TODO: run benchmarks, put pretty bar graphs here
 ## Benchmarks
 ### Serde JSON
-### GJSON
-### A-JSON
+Serde JSON is the most popular JSON parser, with 46 million all-time downloads. It allows integration of JSON values with Rust's typing system; this behavior is not utilized in these benchmarks.
+Serde JSON is an object-parsing library, so the `serde_large_top_level` and `serde_small_top_level` benchmarks provide the most accurate measure of its performance.
 ### json-rust
+json-rust is another object-parsing library with a similar syntax to Serde.
+The `json_rust_large_top_level` and `json_rust_small_top_level` benchmarks provide the most accurate measure of its performance.
+### simd-json
+simd-json is an implementation of the Serde JSON API using x86_64 [SIMD instructions](https://en.wikipedia.org/wiki/SIMD); this means that it will only work on compatible Intel and AMD processors (ARM and other architectures will need to use a different library).
+It is an object-parsing library, meaning that the `simd_large_top_level` and `simd_small_top_level` benchmarks provide the most accurate measure of its performance.
+### tinyjson
+tinyjson is a JSON parsing that uses the `String::parse` API, so you can parse a string to JSON with a syntax as simple as `my_string.parse::<tinyjson::JsonValue>()?`.
+It is an object-parsing library, so the `tinyjson_large_top_level` and `tinyjson_small_top_level` benchmarks provide the most accurate measure of its performance.
+### GJSON
+GJSON is a port of the [Go JSON parser with the same name](https://github.com/tidwall/gjson) by the author of that package.
+It is a property-parsing library, so the `gjson_large_fourth_level`, `gjson_large_top_level`, `gjson_small_fourth_level`, and `gjson_small_top_level` benchmarks are the ones to look at.
+### A-JSON
+A-JSON is a port, by a different author, of Go's GJSON package.
+It is a property-parsing library, so the `ajson_large_fourth_level`, `ajson_large_top_level`, `ajson_small_fourth_level`, and `ajson_small_top_level` benchmarks are the ones to look at.
 ### Pikkr
+Pikkr is a property-parsing library with a unique 'speculative-parsing' approach. Its parser keeps track of state in an effort to make parsing more efficient.
+Since this approach does not work in all situations (for example, concurrent programs must create a new Pikkr instance for each thread), I've benchmarked both `pikkr_stateful` (which uses one Pikkr instance for all benchmark iterations) and `pikkr_stateless` (which creates a new Pikkr instance on each benchmark iteration).
+
+Also, Pikkr's [original repository](https://github.com/melanieseltzer/pikkr/) has not been committed to in 2.5 years, and fails to build on my system. This benchmark uses [my fork](https://crates.io/crates/pikkr-annika), which fixes the build errors but is not maintained (except for reported security vulnerabilities).
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+#![feature(test)]
+
 #[cfg(test)]
 mod tests {
+    extern crate test;
     use lazy_static::lazy_static;
     use std::{fs, path::Path};
+    use test::Bencher;
 
     lazy_static! {
         static ref SMALL_JSON: String = fs::read_to_string(Path::new("json/small.json"))
@@ -10,9 +14,405 @@ mod tests {
             .expect("Error loading 'json/large.json'; make sure you are operating from the root directory");
     }
 
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    // Serde JSON
+    #[bench]
+    fn serde_small_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: serde_json::Value = serde_json::from_str(&SMALL_JSON).unwrap();
+            assert_eq!(parsed["topLevelProperty"], 1);
+        });
+    }
+    #[bench]
+    fn serde_large_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: serde_json::Value = serde_json::from_str(&LARGE_JSON).unwrap();
+            assert_eq!(parsed["topLevelProperty"], 1);
+        });
+    }
+    #[bench]
+    fn serde_small_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: serde_json::Value = serde_json::from_str(&SMALL_JSON).unwrap();
+            assert_eq!(parsed["property"]["subProperty"]["thirdLevel"]["pi"], 3.14159);
+        });
+    }
+    #[bench]
+    fn serde_large_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: serde_json::Value = serde_json::from_str(&LARGE_JSON).unwrap();
+            assert_eq!(parsed["property"]["subProperty"]["thirdLevel"]["pi"], 3.14159);
+        });
+    }
+    #[bench]
+    fn serde_small_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            serde_json::from_str::<serde_json::Value>(&SMALL_JSON).unwrap();
+        });
+    }
+    #[bench]
+    fn serde_large_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            serde_json::from_str::<serde_json::Value>(&LARGE_JSON).unwrap();
+        });
+    }
+
+    // SIMD JSON
+    #[bench]
+    fn simd_small_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: simd_json::OwnedValue = simd_json::serde::from_str(&mut SMALL_JSON.clone()).unwrap();
+            assert_eq!(parsed["topLevelProperty"], 1);
+        });
+    }
+    #[bench]
+    fn simd_large_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: simd_json::OwnedValue = simd_json::serde::from_str(&mut LARGE_JSON.clone()).unwrap();
+            assert_eq!(parsed["topLevelProperty"], 1);
+        });
+    }
+    #[bench]
+    fn simd_small_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: serde_json::Value = simd_json::serde::from_str(&mut SMALL_JSON.clone()).unwrap();
+            // simd-json introduces floating-point precision problems, for some reason
+            assert_eq!(parsed["property"]["subProperty"]["thirdLevel"]["pi"], 3.1415900000000003);
+        });
+    }
+    #[bench]
+    fn simd_large_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: serde_json::Value = simd_json::serde::from_str(&mut LARGE_JSON.clone()).unwrap();
+            assert_eq!(parsed["property"]["subProperty"]["thirdLevel"]["pi"], 3.1415900000000003);
+        });
+    }
+    #[bench]
+    fn simd_small_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            simd_json::serde::from_str::<serde_json::Value>(&mut SMALL_JSON.clone()).unwrap();
+        });
+    }
+    #[bench]
+    fn simd_large_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            simd_json::serde::from_str::<serde_json::Value>(&mut LARGE_JSON.clone()).unwrap();
+        });
+    }
+
+    // GJSON
+    #[bench]
+    fn gjson_small_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: gjson::Value = gjson::get(&SMALL_JSON, "topLevelProperty");
+            assert_eq!(parsed.i32(), 1);
+        });
+    }
+    #[bench]
+    fn gjson_large_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: gjson::Value = gjson::get(&LARGE_JSON, "topLevelProperty");
+            assert_eq!(parsed.i32(), 1);
+        });
+    }
+    #[bench]
+    fn gjson_small_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: gjson::Value = gjson::get(&SMALL_JSON, "property.subProperty.thirdLevel.pi");
+            assert_eq!(parsed.f32(), 3.14159);
+        });
+    }
+    #[bench]
+    fn gjson_large_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: gjson::Value = gjson::get(&LARGE_JSON, "property.subProperty.thirdLevel.pi");
+            assert_eq!(parsed.f32(), 3.14159);
+        });
+    }
+    #[bench]
+    #[ignore = "GJSON's parse() method delays all parsing until a property is accessed, so benchmarking is not feasible"]
+    fn gjson_small_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            gjson::parse(&SMALL_JSON);
+        });
+    }
+    #[bench]
+    #[ignore = "GJSON's parse() method delays all parsing until a property is accessed, so benchmarking is not feasible"]
+    fn gjson_large_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            gjson::parse(&LARGE_JSON);
+        });
+    }
+
+    // A-JSON
+    #[bench]
+    fn ajson_small_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: ajson::Value = ajson::get(&SMALL_JSON, "topLevelProperty").unwrap();
+            assert_eq!(parsed.to_i64(), 1);
+        });
+    }
+    #[bench]
+    fn ajson_large_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: ajson::Value = ajson::get(&LARGE_JSON, "topLevelProperty").unwrap();
+            assert_eq!(parsed.to_i64(), 1);
+        });
+    }
+    #[bench]
+    fn ajson_small_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: ajson::Value = ajson::get(&SMALL_JSON, "property.subProperty.thirdLevel.pi").unwrap();
+            assert_eq!(parsed.to_f64(), 3.14159);
+        });
+    }
+    #[bench]
+    fn ajson_large_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: ajson::Value = ajson::get(&LARGE_JSON, "property.subProperty.thirdLevel.pi").unwrap();
+            assert_eq!(parsed.to_f64(), 3.14159);
+        });
+    }
+    #[bench]
+    #[ignore = "AJSON's parse() method delays about half of the time spent parsing until a property is accessed, so benchmarking is not feasible"]
+    fn ajson_small_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            ajson::parse(&SMALL_JSON).unwrap();
+        });
+    }
+    #[bench]
+    #[ignore = "AJSON's parse() method delays about half of the time spent parsing until a property is accessed, so benchmarking is not feasible"]
+    fn ajson_large_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            ajson::parse(&LARGE_JSON).unwrap();
+        });
+    }
+
+    // json-rust
+    #[bench]
+    fn json_rust_small_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed = json::parse(&SMALL_JSON).unwrap();
+            assert_eq!(parsed["topLevelProperty"], 1);
+        });
+    }
+    #[bench]
+    fn json_rust_large_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed = json::parse(&LARGE_JSON).unwrap();
+            assert_eq!(parsed["topLevelProperty"], 1);
+        });
+    }
+    #[bench]
+    fn json_rust_small_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed = json::parse(&SMALL_JSON).unwrap();
+            assert_eq!(parsed["property"]["subProperty"]["thirdLevel"]["pi"], 3.14159);
+        });
+    }
+    #[bench]
+    fn json_rust_large_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed = json::parse(&LARGE_JSON).unwrap();
+            assert_eq!(parsed["property"]["subProperty"]["thirdLevel"]["pi"], 3.14159);
+        });
+    }
+    #[bench]
+    fn json_rust_small_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            json::parse(&SMALL_JSON).unwrap();
+        });
+    }
+    #[bench]
+    fn json_rust_large_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            json::parse(&LARGE_JSON).unwrap();
+        });
+    }
+
+    // Pikkr
+    #[bench]
+    fn pikkr_stateful_small_top_level(b: &mut Bencher) {
+        let mut parser = pikkr_annika::Pikkr::new(&vec![
+            "$.topLevelProperty".as_bytes(),
+        ], 2).unwrap();
+        b.iter(|| {
+            // Pikkr has a rather low-level API
+            // Maybe someday I'll write a wrapper for it...
+            let parsed: u32 = String::from_utf8(parser.parse(SMALL_JSON.as_bytes())
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .unwrap()
+            .to_vec()).unwrap().parse().unwrap();
+            assert_eq!(parsed, 1);
+        });
+    }
+
+    #[bench]
+    fn pikkr_stateful_large_top_level(b: &mut Bencher) {
+        let mut parser = pikkr_annika::Pikkr::new(&vec![
+            "$.topLevelProperty".as_bytes(),
+        ], 2).unwrap();
+        b.iter(|| {
+            let parsed: u32 = String::from_utf8(parser.parse(LARGE_JSON.as_bytes())
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .unwrap()
+            .to_vec()).unwrap().parse().unwrap();
+            assert_eq!(parsed, 1);
+        });
+    }
+
+    #[bench]
+    fn pikkr_stateless_small_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let mut parser = pikkr_annika::Pikkr::new(&vec![
+                "$.topLevelProperty".as_bytes(),
+            ], 2).unwrap();
+            let parsed: u32 = String::from_utf8(parser.parse(SMALL_JSON.as_bytes())
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .unwrap()
+            .to_vec()).unwrap().parse().unwrap();
+            assert_eq!(parsed, 1);
+        });
+    }
+
+    #[bench]
+    fn pikkr_stateless_large_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let mut parser = pikkr_annika::Pikkr::new(&vec![
+                "$.topLevelProperty".as_bytes(),
+            ], 2).unwrap();
+            let parsed: u32 = String::from_utf8(parser.parse(LARGE_JSON.as_bytes())
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .unwrap()
+            .to_vec()).unwrap().parse().unwrap();
+            assert_eq!(parsed, 1);
+        });
+    }
+    #[bench]
+    fn pikkr_stateful_small_fourth_level(b: &mut Bencher) {
+        let mut parser = pikkr_annika::Pikkr::new(&vec![
+            "$.property.subProperty.thirdLevel.pi".as_bytes(),
+        ], 2).unwrap();
+        b.iter(|| {
+            // Pikkr has a rather low-level API
+            // Maybe someday I'll write a wrapper for it...
+            let parsed: f32 = String::from_utf8(parser.parse(SMALL_JSON.as_bytes())
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .unwrap()
+            .to_vec()).unwrap().parse().unwrap();
+            assert_eq!(parsed, 3.14159);
+        });
+    }
+
+    #[bench]
+    fn pikkr_stateful_large_fourth_level(b: &mut Bencher) {
+        let mut parser = pikkr_annika::Pikkr::new(&vec![
+            "$.property.subProperty.thirdLevel.pi".as_bytes(),
+        ], 2).unwrap();
+        b.iter(|| {
+            let parsed: f32 = String::from_utf8(parser.parse(LARGE_JSON.as_bytes())
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .unwrap()
+            .to_vec()).unwrap().parse().unwrap();
+            assert_eq!(parsed, 3.14159);
+        });
+    }
+
+    #[bench]
+    fn pikkr_stateless_small_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let mut parser = pikkr_annika::Pikkr::new(&vec![
+                "$.property.subProperty.thirdLevel.pi".as_bytes(),
+            ], 2).unwrap();
+            let parsed: f32 = String::from_utf8(parser.parse(SMALL_JSON.as_bytes())
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .unwrap()
+            .to_vec()).unwrap().parse().unwrap();
+            assert_eq!(parsed, 3.14159);
+        });
+    }
+
+    #[bench]
+    fn pikkr_stateless_large_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let mut parser = pikkr_annika::Pikkr::new(&vec![
+                "$.property.subProperty.thirdLevel.pi".as_bytes(),
+            ], 2).unwrap();
+            let parsed: f32 = String::from_utf8_lossy(parser.parse(LARGE_JSON.as_bytes())
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .unwrap()
+            ).parse().unwrap();
+            assert_eq!(parsed, 3.14159);
+        });
+    }
+
+    #[bench]
+    #[ignore = "Pikkr does not support parsing an entire JSON file at once"]
+    fn pikkr_stateful_small_parse_all(_: &mut Bencher) {}
+    #[bench]
+    #[ignore = "Pikkr does not support parsing an entire JSON file at once"]
+    fn pikkr_stateful_large_parse_all(_: &mut Bencher) {}
+    #[bench]
+    #[ignore = "Pikkr does not support parsing an entire JSON file at once"]
+    fn pikkr_stateless_small_parse_all(_: &mut Bencher) {}
+    #[bench]
+    #[ignore = "Pikkr does not support parsing an entire JSON file at once"]
+    fn pikkr_stateless_large_parse_all(_: &mut Bencher) {}
+
+    // tinyjson
+    #[bench]
+    fn tinyjson_small_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: tinyjson::JsonValue = SMALL_JSON.parse().unwrap();
+            assert_eq!(*parsed["topLevelProperty"].get::<f64>().unwrap(), 1.0);
+        });
+    }
+    #[bench]
+    fn tinyjson_large_top_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: tinyjson::JsonValue = LARGE_JSON.parse().unwrap();
+            assert_eq!(*parsed["topLevelProperty"].get::<f64>().unwrap(), 1.0);
+        });
+    }
+    #[bench]
+    fn tinyjson_small_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: tinyjson::JsonValue = SMALL_JSON.parse().unwrap();
+            assert_eq!(*parsed["property"]["subProperty"]["thirdLevel"]["pi"].get::<f64>().unwrap(), 3.14159);
+        });
+    }
+    #[bench]
+    fn tinyjson_large_fourth_level(b: &mut Bencher) {
+        b.iter(|| {
+            let parsed: tinyjson::JsonValue = LARGE_JSON.parse().unwrap();
+            assert_eq!(*parsed["property"]["subProperty"]["thirdLevel"]["pi"].get::<f64>().unwrap(), 3.14159);
+        });
+    }
+    #[bench]
+    fn tinyjson_small_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            SMALL_JSON.parse::<tinyjson::JsonValue>().unwrap();
+        });
+    }
+    #[bench]
+    fn tinyjson_large_parse_all(b: &mut Bencher) {
+        b.iter(|| {
+            LARGE_JSON.parse::<tinyjson::JsonValue>().unwrap();
+        });
     }
 }
